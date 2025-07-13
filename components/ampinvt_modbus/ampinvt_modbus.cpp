5ec8e395657e9e78000f82caff1fae5982df8306
Sebastian Muszynski <basti@linkt.de>
2025-07-13 20:56:57 +0200

    Add checksum

--- components/ampinvt_modbus/ampinvt_modbus.cpp
+++ components/ampinvt_modbus/ampinvt_modbus.cpp
@@ -12,6 +12,14 @@ static const uint8_t AMPINVT_FRAME_SIZE_SETTINGS = 64;
 static const uint8_t AMPINVT_COMMAND_STATUS = 0xB3;
 static const uint8_t AMPINVT_COMMAND_SETTINGS = 0xB2;
 
+uint8_t ampinvt_checksum(const uint8_t data[], const uint8_t len) {
+  uint8_t checksum = 0;
+  for (uint8_t i = 0; i < len; i++) {
+    checksum += data[i];
+  }
+  return checksum;
+}
+
 void AmpinvtModbus::setup() {
   if (this->flow_control_pin_ != nullptr) {
     this->flow_control_pin_->setup();
@@ -64,7 +72,14 @@ bool AmpinvtModbus::parse_ampinvt_modbus_byte_(uint8_t byte) {
   }
 
   if (at == frame_len - 1) {
-    // We have received all expected bytes, process the frame
+    uint8_t computed_checksum = ampinvt_checksum(raw, frame_len - 1);
+    uint8_t remote_checksum = raw[frame_len - 1];
+
+    if (computed_checksum != remote_checksum) {
+      ESP_LOGW(TAG, "Ampinvt checksum failed! %02X!=%02X", computed_checksum, remote_checksum);
+      return false;
+    }
+
     uint8_t address = raw[0];
 
     std::vector<uint8_t> data(this->rx_buffer_.begin(), this->rx_buffer_.end());
