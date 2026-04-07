c6c76a8fc39d9572e75517b6a5ab4ac27a4e41ad
Sebastian Muszynski <basti@linkt.de>
2026-04-07 08:51:17 +0200

    Use STATE_CLASS_MEASUREMENT for today_yield
    today_yield resets daily, so STATE_CLASS_TOTAL_INCREASING would cause
    HA to misinterpret the reset as a counter rollover.

--- components/ampinvt/sensor.py
+++ components/ampinvt/sensor.py
@@ -64,7 +64,7 @@ SENSOR_DEFS = {
         "unit_of_measurement": UNIT_WATT_HOURS,
         "accuracy_decimals": 0,
         "device_class": DEVICE_CLASS_ENERGY,
-        "state_class": STATE_CLASS_TOTAL_INCREASING,
+        "state_class": STATE_CLASS_MEASUREMENT,
     },
     CONF_GENERATION_TOTAL: {
         "unit_of_measurement": UNIT_KILOWATT_HOURS,
